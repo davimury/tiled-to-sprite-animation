09a92ec9c4eb99da447bdcab832b80ceae4992df
Davi Mury <davi.j.mury@gmail.com>
2021-12-02 00:45:54 -0300

    added animation sprite generator, refactor from lua to json

--- export_sprite_from_tiled.py
+++ export_sprite_from_tiled.py
@@ -1,57 +1,93 @@
-import lua_imports; lua_imports.lua_importer.register()
+import os
+import json
 from itertools import product
 from PIL import Image
-import tilemap
-import mapdata
 
 class ExportSprite:
-    def __init__(self) -> None:
-        self.tilemap = {}
-        self.tilemap = self._transform_lua_table_to_dict(tilemap.lua_module[0])
-        self.mapdata = self._transform_lua_table_to_dict(mapdata.lua_module[0])
-        self.matrix = self._get_map_matrix()
-        self.sprite = Image.open(self.tilemap['image'])
-        self.generate_sprite_sheet(1600,1600)
+    def __init__(self, tiled_map_path: str = './tiled_files/tilemap.json', map_data_path: str = './tiled_files/mapdata.json') -> None:
+        self.tilemap = self._load_tiled_map_file(tiled_map_path)
+        self.mapdata = self._load_map_data_file(map_data_path)
+        self.tileset = Image.open('./tiled_files/tileset.png')
+        self.generate_animated_sprite_sheet()
 
-    def _transform_lua_table_to_dict(self, object):
-        tmp = {}
-        for key, value in dict(object).items():
-            if not isinstance(value, str) and not isinstance(value, int):
-                value = self._transform_lua_table_to_dict(value)
-            tmp[key] = value
-        return tmp
+    def _load_tiled_map_file(self, tiled_map_path):
+        with open(tiled_map_path, encoding = 'utf-8') as f:
+            return json.load(f)
+ 
+    def _load_map_data_file(self, map_data_path):
+        with open(map_data_path, encoding = 'utf-8') as f:
+            return json.load(f)
     
     def _get_map_matrix(self):
-        matrix = {}
-        for key, value in self.mapdata['layers'].items():
-            for data_key, data_value in value['data'].items():
-                if data_value != 0:
-                    matrix[data_key] = data_value
-        return matrix
+        matrixes = []
+        for value in self.mapdata['layers']:
+            tmp = {}
+            for data_key, data_value in enumerate(value['data']):
+                tmp[data_key] = data_value
+            matrixes.append(tmp)
+        return matrixes
 
     def _get_tile_by_id(self, value):
-        w, h = self.sprite.size
-        d = 16 # Tamanho
+        w, h = self.tileset.size
+        d = self.tilemap['tilewidth']
         for idx, (i, j) in enumerate(product(range(0, h-h%d, d), range(0, w-w%d, d))):
             if idx == value:
                 box = (j, i, j+d, i+d)
-                return self.sprite.crop(box)
-        
-    def generate_sprite_sheet(self, width, height):
-        matrix = self._get_map_matrix()
-        sprite = Image.new('RGB', (width, height))
-        x_offset = 0
-        y_offset = 0
+                return self.tileset.crop(box)
 
-        for key, value in matrix.items():
-            tile = self._get_tile_by_id(value - 1)
-            if x_offset == width:
-                x_offset = 0
-                y_offset += tile.size[1]
-            sprite.paste(tile, (x_offset, y_offset))
-            x_offset += tile.size[0]
-        sprite.show()
-        return sprite
+    def _save_results(self, image):
+        os.mkdir('./results')
+        image.save(f"./results/result_sprite.png")
+
+    def generate_sprite_sheet(self):
+        matrixes = self._get_map_matrix()
+        sprite_width = self.mapdata['tilewidth'] * self.mapdata['width']
+        sprite_height = self.mapdata['tilewidth'] * self.mapdata['height']
+        sprite = Image.new('RGB', (sprite_width, sprite_height))
+        
+        for matrix in matrixes:
+            x_offset = 0
+            y_offset = 0
+            for key, value in matrix.items():
+                if value != 0:
+                    tile = self._get_tile_by_id(value - 1)
+
+                if x_offset == sprite_width:
+                    x_offset = 0
+                    y_offset += tile.size[1]
+
+                if value != 0:
+                    sprite.paste(tile, (x_offset, y_offset), tile)
+                x_offset += tile.size[0]
+        self._save_results(sprite)
+    
+    def generate_animated_sprite_sheet(self, frames: int = 4):
+        matrixes = self._get_map_matrix()
+        sprite_width = self.mapdata['tilewidth'] * self.mapdata['width']
+        sprite_height = self.mapdata['tilewidth'] * self.mapdata['height']
+        sprite = Image.new('RGB', (sprite_width * frames, sprite_height))
+        
+        for frame in range(frames):
+            for matrix in matrixes:
+                x_offset = 0 + (sprite_width * frame)
+                y_offset = 0
+                for key, value in matrix.items():
+                    tile_id = value - 1
+                    if value != 0:
+                        if "animation" in self.tilemap['tiles'][tile_id]:
+                            tile = self._get_tile_by_id(self.tilemap['tiles'][tile_id]['animation'][frame]['tileid'])
+                        else:
+                            tile = self._get_tile_by_id(tile_id)
+
+                    if x_offset == sprite_width * (frame + 1):
+                        x_offset = 0 + (sprite_width * frame)
+                        y_offset += tile.size[1]
+
+                    if value != 0:
+                        sprite.paste(tile, (x_offset, y_offset), tile)
+
+                    x_offset += tile.size[0]
+        self._save_results(sprite)
 
 ExportSprite()
 
